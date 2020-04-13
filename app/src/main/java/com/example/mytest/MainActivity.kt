9c6d039638d8a1de93af46b6f238dbb19e9b657a
fbi00273 <byun91@pionnet.co.kr>
2020-04-13 16:15:37 +0900

    코틀린3장

--- app/src/main/java/com/example/mytest/MainActivity.kt
+++ app/src/main/java/com/example/mytest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mytest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 import com.example.mytest.RxJava.Rx_Zip
 import com.example.mytest.RxJava.Rx_Zip_Ex3
 
@@ -11,7 +12,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btn.setOnClickListener {
+            btn.text = "클릭했습니다."
+        }
+
+
         //Rx_Zip_Ex3()
+
+
 
 
     }
