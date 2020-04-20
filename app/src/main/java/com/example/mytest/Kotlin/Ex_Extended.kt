73373f138e334164b00dfd60f28321e32ae34c5f
fbi00273 <byun91@pionnet.co.kr>
2020-04-20 13:04:38 +0900

    kotlin ex

--- app/src/main/java/com/example/mytest/Kotlin/Ex_Extended.kt
+++ app/src/main/java/com/example/mytest/Kotlin/Ex_Extended.kt
@@ -4,8 +4,36 @@ class Ex_Extended{
 
     init {
 
+        var str = "dreaming coworker, enjoy working life ,cool company, amazing product"
+        val result = str?.let{
+           // Integer.parseInt(it)
+
+        }
 
 
+
+        with(str){
+            System.out.println("byun"+str)
+
+        }
+
+        System.out.println("byun"+str)
+
+        val c = car()
+
+        val r = c?.apply {
+            c.color = "red"
+        }
+
+        System.out.println(c.color)
     }
+
+    class car{
+        var color : String
+      init {
+          color = "white"
+      }
+    }
+
 
 }
